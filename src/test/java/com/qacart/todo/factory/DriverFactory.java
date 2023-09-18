9fa43037da30c15a766c88a7eeaa68a2f9041996
MohammedNimer <mohammed_nimer_98@yahoo.com>
2023-09-18 23:53:45 +0300

    Update

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -3,13 +3,16 @@ package com.qacart.todo.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class DriverFactory {
     public WebDriver initializeDriver(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver=new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return driver;
